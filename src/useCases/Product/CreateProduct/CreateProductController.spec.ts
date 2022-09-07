d06d83800efb154f39bc728abd2d3389eeb23b3c
lucaspopov@protonmail.com <oLucasPopov>
2022-09-06 23:46:35 -0300

    refactor: request passed to a function

--- src/useCases/Product/CreateProduct/CreateProductController.spec.ts
+++ src/useCases/Product/CreateProduct/CreateProductController.spec.ts
@@ -1,42 +1,38 @@
+import { IHttpRequest } from "../../Presentation/Protocols/http";
 import { CreateProductController } from "./CreateProductController";
+
+const makeFakeRequest = (omit: string): IHttpRequest => {
+
+  const body = {
+    name: 'any_name',
+    price: 'any_price',
+    description: 'any_description',
+    quantity: 'any_quantity',
+  }
+
+  if (omit) {
+    delete body[omit as keyof typeof body];
+  }
+
+  return { body }
+};
 
 describe('Create Product Controller', () => {
   it('should return 400 if no name is provided', async () => {
     const sut = new CreateProductController();
-    const httpRequest = {
-      body: {
-        price: 10,
-        quantity: 10,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest);
-
+    const httpResponse = await sut.handle(makeFakeRequest('name'));
     expect(httpResponse.statusCode).toBe(400);
   })
 
   it('should return 400 if no price is provided', async () => {
     const sut = new CreateProductController();
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        quantity: 10,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest);
-
+    const httpResponse = await sut.handle(makeFakeRequest('price'));
     expect(httpResponse.statusCode).toBe(400);
   })
 
   it('should return 400 if no quantity is provided', async () => {
     const sut = new CreateProductController();
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        price: 10,
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest);
-
+    const httpResponse = await sut.handle(makeFakeRequest('quantity'));
     expect(httpResponse.statusCode).toBe(400);
   })
 })
