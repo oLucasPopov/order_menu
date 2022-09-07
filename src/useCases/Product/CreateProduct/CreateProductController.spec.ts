4302d1f0049dea3c6a3263ba26ad7ae092dcd4b2
lucaspopov@protonmail.com <oLucasPopov>
2022-09-06 23:21:35 -0300

    test: ensure CreateProductController returns 400 if no price is provided

--- src/useCases/Product/CreateProduct/CreateProductController.spec.ts
+++ src/useCases/Product/CreateProduct/CreateProductController.spec.ts
@@ -13,4 +13,17 @@ describe('Create Product Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400);
   })
+
+  it('should return 400 if no price is provided', async () => {
+    const sut = new CreateProductController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        quantity: 10,
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  })
 })
