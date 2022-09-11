0e8025dbb61822090cf268d98fe7132da191d4c8
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 19:51:27 -0300

    test: ensure GetProductController calls GetProductUseCase with correct values

--- src/useCases/Product/GetProduct/GetProductController.spec.ts
+++ src/useCases/Product/GetProduct/GetProductController.spec.ts
@@ -1,3 +1,5 @@
+import { Product } from "../../../entities/Product";
+import { IGetProductUseCase } from "../../Presentation/Protocols/useCases/ProductUseCases";
 import { GetProductController } from "./GetProductController";
 
 const fakeRequest = () => ({
@@ -6,9 +8,53 @@ const fakeRequest = () => ({
   }
 });
 
+const fakeProductData = () => ({
+  name: 'any_name',
+  cost: 10,
+  price: 20,
+  quantity: 30,
+  barcode: 'any_barcode',
+  description: 'any_description',
+  category: 1,
+  unit: 1,
+  expirationDate: new Date(2023, 1, 1, 1, 1, 1, 1),
+  providerCode: 1,
+  ean: 'any_ean',
+  ncm: 'any_ncm',
+  cest: 'any_cest',
+  origin: 'any_origin',
+  liquidWeight: 40,
+  bruteWeight: 50,
+  width: 50,
+  height: 60,
+  length: 70,
+})
+
+const makeFakeProduct = () => ({
+  id: 1,
+  ...fakeProductData()
+});
+
+const makeSut = () => {
+  class GetProductUseCaseStub implements IGetProductUseCase {
+    async execute(id: string): Promise<Product> {
+      const product: Product = new Product();
+      Object.assign(product, { id }, fakeProductData());
+      return Promise.resolve(product);
+    }
+  }
+
+  const getProductUseCase = new GetProductUseCaseStub();
+  const sut = new GetProductController(getProductUseCase);
+  return {
+    getProductUseCase,
+    sut
+  };
+}
+
 describe('Get Product Controller', () => {
   it('Should return 400 if no id is provided', async () => {
-    const sut = new GetProductController();
+    const { sut } = makeSut();
     const httpRequest = {
       params: {}
     }
@@ -16,5 +62,16 @@ describe('Get Product Controller', () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
-  })
+  });
+
+  it('Should call GetProductUseCase with correct id', async () => {
+    const { sut, getProductUseCase } = makeSut();
+    const httpRequest = fakeRequest();
+
+    const executeSpy = jest.spyOn(getProductUseCase, 'execute');
+
+    await sut.handle(httpRequest);
+
+    expect(executeSpy).toHaveBeenCalledWith(1);
+  });
 });
