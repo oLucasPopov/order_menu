c40465de7d0dc9580eed3283f1d77d6f309fe690
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 19:51:14 -0300

    feat: ensure GetProductController calls GetProductUseCase with correct values

--- src/useCases/Product/GetProduct/GetProductController.ts
+++ src/useCases/Product/GetProduct/GetProductController.ts
@@ -2,13 +2,22 @@ import { MissingParamError } from "../../Presentation/errors/MissingParamError";
 import { badRequest } from "../../Presentation/helpers/http/httpHelper";
 import { IController } from "../../Presentation/Protocols/controller";
 import { IHttpRequest, IHttpResponse } from "../../Presentation/Protocols/http";
+import { IGetProductUseCase } from "../../Presentation/Protocols/useCases/ProductUseCases";
 
 
 export class GetProductController implements IController {
+  constructor(
+    private getProductUseCase: IGetProductUseCase
+  ) {}
+
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
     if(!request.params.id) {
       return badRequest(new MissingParamError('id'))
     }
+
+    const { id } = request.params;
+
+    const product = await this.getProductUseCase.execute(id);
 
     return Promise.resolve({
       statusCode: 200,
