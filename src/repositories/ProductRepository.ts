ecdafcfd5401e0392289c1762d3fc7054612797b
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 19:49:25 -0300

    feat: create IGetProductRepository interface

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -3,3 +3,7 @@ import { AddProduct, Product } from "../entities/Product";
 export interface ICreateProductRepository {
   create(data: AddProduct): Promise<Product>;
 }
+
+export interface IGetProductRepository {
+  get(id: string): Promise<Product>;
+}
