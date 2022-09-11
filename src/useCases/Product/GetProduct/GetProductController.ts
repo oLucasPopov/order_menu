a344d6b3762411c9a709dae55c3acf1279424831
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 19:56:29 -0300

    feat: Should return 404 if GetProductUseCase returns null

--- src/useCases/Product/GetProduct/GetProductController.ts
+++ src/useCases/Product/GetProduct/GetProductController.ts
@@ -1,5 +1,6 @@
 import { MissingParamError } from "../../Presentation/errors/MissingParamError";
-import { badRequest, ok } from "../../Presentation/helpers/http/httpHelper";
+import { NotFoundError } from "../../Presentation/errors/NotFoundError";
+import { badRequest, notFound, ok } from "../../Presentation/helpers/http/httpHelper";
 import { IController } from "../../Presentation/Protocols/controller";
 import { IHttpRequest, IHttpResponse } from "../../Presentation/Protocols/http";
 import { IGetProductUseCase } from "../../Presentation/Protocols/useCases/ProductUseCases";
@@ -18,6 +19,10 @@ export class GetProductController implements IController {
     const { id } = request.params;
 
     const product = await this.getProductUseCase.execute(id);
+
+    if (!product) {
+      return notFound(new NotFoundError('Product not found'));
+    }
 
     return ok(product);
   }
