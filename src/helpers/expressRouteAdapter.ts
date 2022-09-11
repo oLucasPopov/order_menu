32c10a9d921c6a51cdfda5d150a2073250b65f21
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 18:42:36 -0300

    feat: Create adapter for routes

---
+++ src/helpers/expressRouteAdapter.ts
@@ -0,0 +1,21 @@
+import { IHttpRequest, IHttpResponse } from "../useCases/Presentation/Protocols/http";
+import { Request, RequestHandler, Response } from 'express'
+import { IController } from "../useCases/Presentation/Protocols/controller";
+
+export const adaptRoute = (controller: IController): RequestHandler => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: IHttpRequest = {
+      body: req.body
+    }
+    const httpResponse: IHttpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      console.log(httpResponse.body)
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+      
+    }
+  }
+};
