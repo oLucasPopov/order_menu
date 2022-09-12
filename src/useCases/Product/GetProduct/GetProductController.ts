8162dfb047f32a9a12d6a4d55cff2be5c79a091b
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 22:06:12 -0300

    feat: add try/catch to GetProductController

--- src/useCases/Product/GetProduct/GetProductController.ts
+++ src/useCases/Product/GetProduct/GetProductController.ts
@@ -1,6 +1,6 @@
 import { MissingParamError } from "../../Presentation/errors/MissingParamError";
 import { NotFoundError } from "../../Presentation/errors/NotFoundError";
-import { badRequest, notFound, ok } from "../../Presentation/helpers/http/httpHelper";
+import { badRequest, notFound, ok, serverError } from "../../Presentation/helpers/http/httpHelper";
 import { IController } from "../../Presentation/Protocols/controller/controller";
 import { IHttpRequest, IHttpResponse } from "../../Presentation/Protocols/http/http";
 import { IGetProductUseCase } from "../../Presentation/Protocols/useCases/ProductUseCases";
@@ -9,21 +9,25 @@ import { IGetProductUseCase } from "../../Presentation/Protocols/useCases/Produc
 export class GetProductController implements IController {
   constructor(
     private getProductUseCase: IGetProductUseCase
-  ) {}
+  ) { }
 
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    if(!request.params.id) {
-      return badRequest(new MissingParamError('id'))
+    try {
+      if (!request.params.id) {
+        return badRequest(new MissingParamError('id'))
+      }
+
+      const { id } = request.params;
+
+      const product = await this.getProductUseCase.execute(id);
+
+      if (!product) {
+        return notFound(new NotFoundError('Product'));
+      }
+
+      return ok(product);
+    } catch (error: any) {
+      return serverError(error);
     }
-
-    const { id } = request.params;
-
-    const product = await this.getProductUseCase.execute(id);
-    
-    if (!product) {
-      return notFound(new NotFoundError('Product'));
-    }
-
-    return ok(product);
   }
 }
