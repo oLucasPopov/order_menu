be7ab1516acff88b0ae0a2b5a24812e8169860d5
lucaspopov@protonmail.com <oLucasPopov>
2022-09-11 21:36:06 -0300

    feat: create route for getProduct

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { createProductController } from "./useCases/Product/CreateProduct"
 import { adaptRoute } from "./helpers/expressRouteAdapter"
+import { getProductController } from "./useCases/Product/GetProduct";
 
 
 const router = Router()
 
-router.post('/products', adaptRoute(createProductController))
+router.post('/products', adaptRoute(createProductController));
+router.get('/products/:id', adaptRoute(getProductController));
 
 export { router }	
