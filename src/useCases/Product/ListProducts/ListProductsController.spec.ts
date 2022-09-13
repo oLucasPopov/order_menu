91cc6462c44b7d4cbfe0de54eb4fcc927c0476d5
lucaspopov@protonmail.com <oLucasPopov>
2022-09-12 21:49:57 -0300

    test: ensure ListProductsController returns MissingHeaderError when items per page is not provided

--- src/useCases/Product/ListProducts/ListProductsController.spec.ts
+++ src/useCases/Product/ListProducts/ListProductsController.spec.ts
@@ -45,4 +45,16 @@ describe('ListProductsController', () => {
     expect(httpResponse.body).toEqual(new MissingHeaderError('x-current-page'));
   });
 
+  it('should return 400 if no items per page is provided', async () => {
+    const { sut } = makeSut();
+ 
+    const httpResponse = await sut.handle({
+      headers: {
+        "Content-Type": "application/json",
+        "x-current-page": 1,
+      }
+    });
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
 });
