a0ecf2df782c448d020b09fbcc8a8a56eac8e134
lucaspopov@protonmail.com <oLucasPopov>
2022-09-13 18:22:32 -0300

    refactor: add index to presentation errors

---
+++ src/useCases/Presentation/errors/index.ts
@@ -0,0 +1,11 @@
+import { InvalidParamError } from "./InvalidParamError";
+import { MissingParamError } from "./MissingParamError";
+import { NotFoundError } from "./NotFoundError";
+import { MissingHeaderError } from "./MissingHeaderError";
+
+export {
+  InvalidParamError,
+  MissingParamError,
+  NotFoundError,
+  MissingHeaderError
+}; 
