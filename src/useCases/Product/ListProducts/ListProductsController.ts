5153698dfa7aab499a23f9753834b528bd5ad32b
lucaspopov@protonmail.com <oLucasPopov>
2022-09-12 21:44:08 -0300

    feat: ensure ListProductsController returns 400 if current page is not provided

--- src/useCases/Product/ListProducts/ListProductsController.ts
+++ src/useCases/Product/ListProducts/ListProductsController.ts
@@ -1,10 +1,16 @@
-import { ok } from "../../Presentation/helpers/http/httpHelper";
+import { MissingHeaderError } from "../../Presentation/errors/MissingHeaderError";
+import { badRequest, ok } from "../../Presentation/helpers/http/httpHelper";
 import { IController, IHttpRequest, IHttpResponse } from "../../Presentation/Protocols";
 
 
 export class ListProductsController implements IController {
 
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
+
+    if(!request.headers["x-current-page"]) {
+      return badRequest(new MissingHeaderError("x-current-page"));
+    }
+
     return ok([]);
   }
 }
