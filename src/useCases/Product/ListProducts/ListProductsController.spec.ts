931b07d4576739e4eeffac7c3007efaf5f283a93
lucaspopov@protonmail.com <oLucasPopov>
2022-09-12 21:44:20 -0300

    test: ensure ListProductsController returns 400 if current page is not provided

--- src/useCases/Product/ListProducts/ListProductsController.spec.ts
+++ src/useCases/Product/ListProducts/ListProductsController.spec.ts
@@ -5,10 +5,31 @@ const makeSut = () => {
   return { sut };
 };
 
+const makeFakeRequest = () => ({
+  headers: {
+    "Content-Type": "application/json",
+    "x-current-page": 1,
+    "x-items-per-page": 20,
+  }
+});
+
 describe('ListProductsController', () => {
   it('should return 200 on success', async () => {
     const { sut } = makeSut();
-    const httpResponse = await sut.handle({});
+    const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse.statusCode).toBe(200);
   });
+
+  it('should return 400 if no page is provided', async () => {
+    const { sut } = makeSut();
+ 
+    const httpResponse = await sut.handle({
+      headers: {
+        "Content-Type": "application/json",
+        "x-items-per-page": 20,
+      }
+    });
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  
 });
