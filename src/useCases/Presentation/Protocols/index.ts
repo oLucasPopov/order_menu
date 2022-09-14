024ecb716595dbed821371139a9131650526960f
lucaspopov@protonmail.com <oLucasPopov>
2022-09-13 23:12:41 -0300

    feat: Create IPagination interface

--- src/useCases/Presentation/Protocols/index.ts
+++ src/useCases/Presentation/Protocols/index.ts
@@ -1,4 +1,5 @@
 import { IController } from './controller/controller';
 import { IHttpRequest, IHttpResponse } from './http/http';
+import { IPagination } from './pagination/pagination';
 
-export { IController, IHttpRequest, IHttpResponse };
+export { IController, IHttpRequest, IHttpResponse, IPagination };
