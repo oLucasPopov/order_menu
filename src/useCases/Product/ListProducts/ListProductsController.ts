563da6384aca71eade16be8e861d64bd061b50f1
lucaspopov@protonmail.com <oLucasPopov>
2022-09-13 22:44:46 -0300

    feat: ensure ListProductsController returns 500 if ListProductsUseCase throws

--- src/useCases/Product/ListProducts/ListProductsController.ts
+++ src/useCases/Product/ListProducts/ListProductsController.ts
@@ -1,25 +1,37 @@
-import { InvalidParamError, MissingHeaderError} from "../../Presentation/errors";
+import { InvalidParamError, MissingHeaderError } from "../../Presentation/errors";
 import { badRequest, ok } from "../../Presentation/helpers/http/httpHelper";
 import { IController, IHttpRequest, IHttpResponse } from "../../Presentation/Protocols";
+import { IListProductUseCase } from "../../Presentation/Protocols/useCases/ProductUseCases";
+import { serverError } from "../../Presentation/helpers/http/httpHelper";
 
 
 export class ListProductsController implements IController {
 
+  constructor(
+    private listProductUseCase: IListProductUseCase
+  ) { }
+
   async handle(request: IHttpRequest): Promise<IHttpResponse> {
-    const requiredHeaders = ['x-current-page', 'x-items-per-page'];
+    try {
+      const requiredHeaders = ['x-current-page', 'x-items-per-page'];
 
-    for(let header of requiredHeaders) {
-      if(!request.headers[header]) {
-        return badRequest(new MissingHeaderError(header));
+      for (let header of requiredHeaders) {
+        if (!request.headers[header]) {
+          return badRequest(new MissingHeaderError(header));
+        }
       }
-    }
 
-    for(let header of requiredHeaders) {
-      if(typeof request.headers[header] !== 'number') {
-        return badRequest(new InvalidParamError(header));
+      for (let header of requiredHeaders) {
+        if (typeof request.headers[header] !== 'number') {
+          return badRequest(new InvalidParamError(header));
+        }
       }
-    }
 
-    return ok([]);
+      const products = this.listProductUseCase.execute();
+
+      return ok(products);
+    } catch (error: any) {
+      return serverError(error.message);
+    }
   }
 }
