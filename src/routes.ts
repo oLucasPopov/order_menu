758b8ef7dbf859580dc03c11994327db6bfd27e8
lucaspopov@protonmail.com <oLucasPopov>
2022-09-15 20:14:22 -0300

    feat: create updateProductController route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { createProductController } from "./useCases/Product/CreateProduct"
 import { adaptRoute } from "./helpers/expressRouteAdapter"
 import { getProductController } from "./useCases/Product/GetProduct";
 import { listProductsController } from "./useCases/Product/ListProducts";
+import { updateProductController } from "./useCases/Product/UpdateProduct";
 
 
 const router = Router()
@@ -10,5 +11,6 @@ const router = Router()
 router.post('/products', adaptRoute(createProductController));
 router.get('/products/:id', adaptRoute(getProductController));
 router.get('/products', adaptRoute(listProductsController));
+router.put('/products/:id', adaptRoute(updateProductController));
 
 export { router }	
