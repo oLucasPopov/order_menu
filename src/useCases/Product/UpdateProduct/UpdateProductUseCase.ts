0b9004f845d1cebe3a6d2c3840f2d1e1b97c25ea
lucaspopov@protonmail.com <oLucasPopov>
2022-09-15 19:18:49 -0300

    feat: ensure UpdateProtuctUseCases calls updateProductRepository with correct values

--- src/useCases/Product/UpdateProduct/UpdateProductUseCase.ts
+++ src/useCases/Product/UpdateProduct/UpdateProductUseCase.ts
@@ -1,9 +1,11 @@
 import { Product, UpdateProduct } from "../../../entities/Product";
+import { IUpdateProductRepository } from "../../../repositories/ProductRepository";
 import { IUpdateProductUseCase } from "../ProductUseCases";
 
 
 export class UpdateProductUseCase implements IUpdateProductUseCase {
+  constructor(private readonly updateProductRepository: IUpdateProductRepository) {}
   async execute(product: UpdateProduct, productId: number): Promise<Product> {
-    return new Product()
+    return this.updateProductRepository.update(productId, product);
   }
 }
