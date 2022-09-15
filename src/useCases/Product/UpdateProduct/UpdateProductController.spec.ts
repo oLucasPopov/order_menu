44f2437ffaad8fa5a40c153419aab408e65fe6b1
lucaspopov@protonmail.com <oLucasPopov>
2022-09-15 10:46:08 -0300

    test: ensure UpdateProductController returns 200 if valid id is provided

--- src/useCases/Product/UpdateProduct/UpdateProductController.spec.ts
+++ src/useCases/Product/UpdateProduct/UpdateProductController.spec.ts
@@ -13,4 +13,15 @@ describe('UpdateProductController', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('id')));
   })
+
+  it('should return 200 if valid id is provided', async () => {
+    const sut = new UpdateProductController()
+    const httpRequest = {
+      params: {
+        id: 'any_id'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200);
+  })
 });
